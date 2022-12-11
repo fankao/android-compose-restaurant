85c4ad506ed71a6b299ac3a25c34dd762628d799
Chien Nguyen <36409877+fankao@users.noreply.github.com>
2022-12-11 09:06:17 +0700

    Complete chapter 3

---
+++ app/src/main/java/com/example/android_compose_restaurant/RestaurantsApiService.kt
@@ -0,0 +1,9 @@
+package com.example.android_compose_restaurant
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface RestaurantsApiService {
+    @GET("restaurants.json")
+    fun getRestaurants(): Call<List<Restaurant>>
+}
